451fdb22da03eb51386e011de1e34b203143a71d
Mike Blume <mike@loggly.com>
2014-01-30 17:20:32 -0800

    simplify slightly

--- src/main/java/deadlock/DeadLockMain.java
+++ src/main/java/deadlock/DeadLockMain.java
@@ -2,7 +2,6 @@ package deadlock;
 
 import clojure.lang.PersistentHashSet;
 import clojure.lang.RT;
-import clojure.lang.Symbol;
 
 public class DeadLockMain {
 
@@ -10,7 +9,7 @@ public class DeadLockMain {
     new Thread(new Runnable() {
 
       public void run() {
-        RT.var("clojure.core", "require").invoke(Symbol.intern("deadlock"));
+        RT.var("clojure.core", "require");
       }
       
     }).start();
